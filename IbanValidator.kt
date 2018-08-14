93d82bc1b6e73ca4d9e0eafc33b2fce54bf282e3
Abdullah Alhazmy <me@alhazmy13.net>
2018-08-15 02:57:11 +0300

    update validator implementation

--- IbanValidator.kt
+++ IbanValidator.kt
@@ -8,8 +8,9 @@ fun isValidIban(iban: String): Boolean {
         return false
     }
     val swapped = symbols.substring(4) + symbols.substring(0, 4)
-    return swapped.chars()
-            .reduce(0) { previousMod: Int, _char: Int ->
+    return swapped.toCharArray()
+            .map { it.toInt() }
+            .fold(0) { previousMod: Int, _char: Int ->
                 val value = Integer.parseInt(Character.toString(_char.toChar()), 36)
                 val factor = if (value < 10) 10 else 100
                 (factor * previousMod + value) % 97
